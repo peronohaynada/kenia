c1196f23adefdb9ef3f77476968cbd166276ff44
drmattmad <image.mathias@gmail.com>
2014-02-03 22:27:50 -0200

    removiendo archivos duplicados de mal commit

--- sokker/sql_scripts/countries.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists countries (
-	country_id smallint(100) not null primary key,
-	name varchar(256) not null,
-	currency_name varchar(256) not null,
-	currency_rate varchar(256) not null
-)
--- sokker/xml.objects/xml.interface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface XmlI {
-	public function loadFromXML(SimpleXMLElement $node);
-	public function insertIntoDB(/*PDO &$db*/);
-	public function getNodeName();
-}
