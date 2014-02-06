9fa825892939a4f8de2779d44aa632278d474843
drmattmad <image.mathias@gmail.com>
2014-02-06 00:20:51 -0200

    Logger class

--- sokker/sokker/errors/errors.control.php
+++ sokker/sokker/errors/errors.control.php
@@ -1,1 +1,9 @@
 <?php
+require_once 'util/constant.definition.php';
+
+class Logger {
+	public static function logWarning($message) {
+		$currentTime = "[".date("d-M-Y H:i:s", time())."] ";
+		error_log($currentTime.$message."\n", 3, WARNING_LOG);
+	}
+}
